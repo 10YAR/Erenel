4d2efea041d45355b025b6dd7f8bc264c9b89b51
Diyar <diyar@digiplace.fr>
2023-04-11 02:41:50 +0200

    assets

--- wp-content/themes/taxseco-child/functions.php
+++ wp-content/themes/taxseco-child/functions.php
@@ -13,6 +13,14 @@
  */
 function taxseco_child_enqueue_styles() {
     wp_enqueue_style( 'taxseco-style', get_template_directory_uri() . '/style.css?v='.time() );
-    wp_enqueue_style( 'taxseco-child-style', get_stylesheet_directory_uri() . '/style.css?v='.time(),array( 'taxseco-style' ),wp_get_theme()->get('Version'));
+    wp_enqueue_style( 'taxseco-child-style', get_stylesheet_directory_uri() . '/style.css?v='.time(),array( 'taxseco-style' ), wp_get_theme()->get('Version'));
 }
 add_action( 'wp_enqueue_scripts', 'taxseco_child_enqueue_styles', 100000 );
+
+function erenel_enqueue_scripts() {
+    wp_enqueue_script( 'flatpickr', get_stylesheet_directory_uri() . '/js/flatpickr.js', array( 'jquery' ), wp_get_theme()->get('Version'), true );
+    wp_enqueue_script( 'flatpickr-fr', get_stylesheet_directory_uri() . '/js/flatpickr-fr.js', array( 'jquery', 'flatpickr' ), wp_get_theme()->get('Version'), true );
+    wp_enqueue_script( 'momentjs', get_stylesheet_directory_uri() . '/js/moment.js', array( 'jquery' ), wp_get_theme()->get('Version'), true );
+    wp_enqueue_script( 'booking', get_stylesheet_directory_uri() . '/js/booking.js?v='.time(), array( 'jquery', 'flatpickr', 'momentjs' ), wp_get_theme()->get('Version'), true );
+}
+add_action( 'wp_enqueue_scripts', 'erenel_enqueue_scripts' );
--- wp-content/themes/taxseco-child/js/booking.js
+++ wp-content/themes/taxseco-child/js/booking.js
@@ -36,9 +36,9 @@ function initializeMap(callback) {
     });
 }
 
-$(document).ready(function () {
+jQuery(document).ready(function () {
     const today = new Date();
-    $("#flatpickr_date").flatpickr({
+    jQuery("#flatpickr_date").flatpickr({
         position:" center",
         monthSelectorType: "static",
         enableTime: false,
@@ -46,7 +46,7 @@ $(document).ready(function () {
         minDate: today,
         locale: "fr"
     });
-    $("#flatpickr_time").flatpickr({
+    jQuery("#flatpickr_time").flatpickr({
         position:" center",
         enableTime: true,
         noCalendar: true,
@@ -55,31 +55,31 @@ $(document).ready(function () {
         locale: "fr"
     });
 
-    $("#flatpickr_date, #flatpickr_time").change(function() {
+    jQuery("#flatpickr_date, #flatpickr_time").change(function() {
         checkPrice();
     });
 });
 
 function checkPrice() {
-    $("#mark_tarif, #mark_distance, #mark_duration").hide();
-    $("#trip_continue_button").attr("disabled","disabled");
-    $("#trip_pick_place_id").val("");
-    $("#trip_drop_place_id").val("");
-    $("#trip_pick_address").val("");
-    $("#trip_drop_address").val("");
-    $("#trip_error").html("");
+    jQuery("#mark_tarif, #mark_distance, #mark_duration").hide();
+    jQuery("#trip_continue_button").attr("disabled","disabled");
+    jQuery("#trip_pick_place_id").val("");
+    jQuery("#trip_drop_place_id").val("");
+    jQuery("#trip_pick_address").val("");
+    jQuery("#trip_drop_address").val("");
+    jQuery("#trip_error").html("");
 
-    const depart = $("#autocomplete_depart").val();
-    const arrivee = $("#autocomplete_arrivee").val();
-    const date = $("#flatpickr_date").val();
-    const time = $("#flatpickr_time").val();
-    const allerretour = $("#input_aller_retour").val();
+    const depart = jQuery("#autocomplete_depart").val();
+    const arrivee = jQuery("#autocomplete_arrivee").val();
+    const date = jQuery("#flatpickr_date").val();
+    const time = jQuery("#flatpickr_time").val();
+    const allerretour = jQuery("#input_aller_retour").val();
 
     let place_depart = autocomplete_depart.getPlace();
     let place_arrivee = autocomplete_arrivee.getPlace();
 
     if (depart && arrivee && date && time && place_depart.place_id && place_arrivee.place_id) {
-        $.post("/xhr.calculate_trip.php",
+        jQuery.post("/xhr.calculate_trip.php",
             {
                 depart: place_depart.place_id,
                 arrivee: place_arrivee.place_id,
@@ -92,18 +92,18 @@ function checkPrice() {
                 let text_aller = (allerretour == "true") ? "(Aller-retour)" : "(Aller simple)";
 
                 if (data.distance && data.price && !data.error) {
-                    $("#mark_distance").html("Distance: " + data.distance + " " + text_aller);
-                    $("#mark_tarif").html("Tarif: " + data.price + " €");
-                    $("#mark_duration").html("Durée trajet: " + data.text_duration);
-                    $("#mark_tarif, #mark_distance, #mark_duration").show();
-                    $("#trip_continue_button").removeAttr("disabled");
+                    jQuery("#mark_distance").html("Distance: " + data.distance + " " + text_aller);
+                    jQuery("#mark_tarif").html("Tarif: " + data.price + " €");
+                    jQuery("#mark_duration").html("Durée trajet: " + data.text_duration);
+                    jQuery("#mark_tarif, #mark_distance, #mark_duration").show();
+                    jQuery("#trip_continue_button").removeAttr("disabled");
 
-                    $("#trip_pick_place_id").val(place_depart.place_id);
-                    $("#trip_drop_place_id").val(place_arrivee.place_id);
-                    $("#trip_pick_address").val(place_depart.formatted_address);
-                    $("#trip_drop_address").val(place_arrivee.formatted_address);
+                    jQuery("#trip_pick_place_id").val(place_depart.place_id);
+                    jQuery("#trip_drop_place_id").val(place_arrivee.place_id);
+                    jQuery("#trip_pick_address").val(place_depart.formatted_address);
+                    jQuery("#trip_drop_address").val(place_arrivee.formatted_address);
                 }else if (data.error) {
-                    $("#trip_error").html(data.message.toString());
+                    jQuery("#trip_error").html(data.message.toString());
                 }
             });
     }
