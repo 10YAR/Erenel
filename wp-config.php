f772affefb4d9da2f143c1c162f8b08ef5f1b0c6
Diyar <diyar@digiplace.fr>
2023-04-11 02:28:14 +0200

    lot of stuff

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,7 @@
 <?php
+require_once "vendor/autoload.php";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 /**
  * The base configuration for WordPress
  *
@@ -20,16 +23,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'erenel' );
+define( 'DB_NAME', $_ENV['DB_NAME'] );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', $_ENV['DB_USER'] );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', $_ENV['DB_HOST'] );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
